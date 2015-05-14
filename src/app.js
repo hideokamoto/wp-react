9423010c0af744623a1cff9027a7603ed3466aea
hideokamoto <kokkoku214@gmail.com>
2015-05-15 01:46:55 +0900

    buxfix/activate RouteHandler

--- src/app.js
+++ src/app.js
@@ -1,39 +1,27 @@
 var React = require('react');
 var Router = require('react-router');
-var Route = Router.Route;
-var NotFoundRoute = Router.NotFoundRoute;
 var DefaultRoute = Router.DefaultRoute;
 var Link = Router.Link;
+var Route = Router.Route;
 var RouteHandler = Router.RouteHandler;
+var NotFoundRoute = Router.NotFoundRoute;
 
-var App = React.createClass({
+
+var Dashboard = React.createClass({
   render: function(){
     return(
-      <div>
-        <header>
-          <ul>
-            <li><Link to="app">Dashboard</Link></li>
-            <li><Link to="inbox">Inbox</Link></li>
-            <li><Link to="calendar">Calendar</Link></li>
-          </ul>
-          Logged in as Jane
-        </header>
-        <RouteHandler />
-      </div>
-    )
+      <div>Dashboard</div>
+    );
   }
 });
 
-var routes = (
-  <Route name="app" path="/" handler={App}>
-    <Route name="inbox" handler={Inbox} />
-    <Route name="calendar" handler={Calendar} />
-    <DefaultRoute handler={Dashboard} />
-  </Route>
-);
 
-Router.run(routes, function(Handler){
-  React.render(<Handler />, document.body);
+var NotFound = React.createClass({
+    render: function(){
+      return (
+        <div className="hode">not</div>
+      );
+    }
 });
 
 var Inbox = React.createClass({
@@ -52,10 +40,32 @@ var Calendar = React.createClass({
   }
 });
 
-var Dashboard = React.createClass({
+var App = React.createClass({
   render: function(){
     return(
-      <div>Dashboard</div>
-    );
+      <div>
+        <header>
+          <ul>
+            <li><Link to="app">Dashboard</Link></li>
+            <li><Link to="inbox">Inbox</Link></li>
+            <li><Link to="calendar">Calendar</Link></li>
+          </ul>
+        </header>
+        <RouteHandler/>
+      </div>
+    )
   }
+});
+
+var routes = (
+  <Route name="app" path="/" handler={App}>
+    <Route name="inbox" handler={Inbox} />
+    <Route name="calendar" handler={Calendar} />
+    <DefaultRoute handler={Dashboard} />
+    <NotFoundRoute handler={NotFound}/>
+  </Route>
+);
+
+Router.run(routes, function(Handler){
+  React.render(<Handler />, document.body);
 });
